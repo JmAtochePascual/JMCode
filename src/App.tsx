e4e638c6e6840bcc3d3d778f3aa95f02f0e2458e
JeanCarlos <jmatochepascual@gmail.com>
2025-01-09 18:56:02 -0500

    feat: :sparkles: Inicia tailwindcss
    Instala tailwindcss para los estilos del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold text-blue-600 text-center mt-8">
+        Hello World
+      </h1>
     </>
   )
 }
