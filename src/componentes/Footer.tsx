fe73d0bd0d0a24df17e2ea178737fd57df25952c
JeanCarlos <jmatochepascual@gmail.com>
2025-01-11 11:50:24 -0500

    feat: :sparkles: Actualiza enlaces en el componente Footer para navegación interna
    Se modifican los enlaces en el componente Footer, cambiando las rutas absolutas por identificadores de secciones. Esto mejora la navegación interna del portafolio al permitir un desplazamiento directo a las secciones correspondientes, optimizando la experiencia del usuario.

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
     <footer className="bg-[#090D18] py-10 md:py-4">
       <div className="w-11/12 max-w-7xl mx-auto py-10 flex flex-col items-center gap-4 md:flex-row md:justify-between md:py-4">
         <nav className="flex flex-col items-center gap-4 md:flex-row">
-          <a href="/" className="text-white text-lg">Inicio</a>
-          <a href="/" className="text-white text-lg">Experiencias</a>
-          <a href="/" className="text-white text-lg">Proyectos</a>
-          <a href="/" className="text-white text-lg">Sobre mí</a>
-          <a href="/" className="text-white text-lg">Contato</a>
+          <a href="#hero" className="text-white text-lg">Inicio</a>
+          <a href="#experiencia" className="text-white text-lg">Experiencias</a>
+          <a href="#proyectos" className="text-white text-lg">Proyectos</a>
+          <a href="#about" className="text-white text-lg">Sobre mí</a>
+          <a href="#testimonios" className="text-white text-lg">Testimonios</a>
         </nav>
 
         <p className="hidden md:block text-white text-lg font-bold">JMCode</p>
