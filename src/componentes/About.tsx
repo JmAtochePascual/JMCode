acaf421301f8a380d1c7e4d0db93861fe5b93acf
JeanCarlos <jmatochepascual@gmail.com>
2025-01-11 11:07:39 -0500

    feat: :sparkles: Actualiza el componente About con nuevo contenido y diseño
    Se añade una nueva imagen 'about.jpg' y se elimina 'about1.jpg'. Además, se actualiza el contenido textual del componente About para reflejar la experiencia y habilidades del desarrollador, mejorando la presentación visual y la claridad del mensaje. Se incluye un botón para descargar el CV, optimizando la interacción del usuario.

--- src/componentes/About.tsx
+++ src/componentes/About.tsx
@@ -9,21 +9,24 @@ const About = () => {
 
       <div className="grid md:grid-cols-2 gap-8 items-center">
         <div className="flex flex-col gap-4">
-          <p className="text-gray-400">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          <p className="text-white leading-8">¡Hola! Soy Jean Carlos, un desarrollador web y diseñador UX/UI apasionado por crear soluciones digitales completas. Con conocimientos en Frontend, Backend y Diseño UX/UI, domino herramientas como React, Node.js y Tailwind CSS, además de metodologías ágiles para garantizar un flujo de trabajo eficiente en equipo.
           </p>
 
-          <p className="text-gray-400 hidden lg:block">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          <p className="text-white hidden lg:block leading-8">Me destaco no solo por mis habilidades técnicas, sino también por mis habilidades blandas: comunicación efectiva, liderazgo colaborativo y una gran capacidad para resolver problemas. Creo firmemente que un buen diseño y un desarrollo sólido son claves para transformar ideas en productos digitales exitosos.
+
+            Si buscas alguien que combine creatividad, técnica y un enfoque centrado en el usuario, ¡hablemos y hagamos algo increíble juntos!
           </p>
         </div>
 
         <img
-          src="./about1.jpg"
+          src="./about.jpg"
           alt="Sobre mí"
-          className="w-full h-auto rounded-lg" />
-
+          className="w-full h-full rounded-lg object-cover loading-lazy" />
       </div>
+
+      <button className="w-full mt-4 bg-primary-color text-white px-4 py-2 rounded-md hover:bg-primary-color/80 shadow-2xl md:w-auto">
+        Descargar CV
+      </button>
     </section>
   )
 }
