373e399d136003a58d3775ea8f89adb7ccebab2c
JeanCarlos <jmatochepascual@gmail.com>
2025-01-11 17:39:16 -0500

    fix: :bug: Ajusta la clase del enlace de descarga en el componente Hero
    Se modifica la clase del enlace de descarga del currículum en el componente Hero para incluir 'text-center', mejorando la alineación del texto. Este cambio optimiza la presentación visual del enlace y la experiencia del usuario al interactuar con el portafolio.

--- src/componentes/Hero.tsx
+++ src/componentes/Hero.tsx
@@ -71,7 +71,7 @@ const Hero = () => {
           <a
             href="Curriculum Jeancarlos Atoche Pascual.pdf"
             download
-            className="bg-primary-color text-white px-4 py-2 rounded-md hover:bg-primary-color/80 shadow-2xl hover:scale-125 transition-all duration-300">
+            className="bg-primary-color text-white px-4 py-2 rounded-md hover:bg-primary-color/80 shadow-2xl hover:scale-125 transition-all duration-300 text-center">
             Descargar CV
           </a>
         </div>
