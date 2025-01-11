5c1d8f4ba52ff5debf91cdf13f709d6a45a36628
JeanCarlos <jmatochepascual@gmail.com>
2025-01-11 16:37:01 -0500

    feat: :sparkles: Añade el componente react-countup para animar contadores en Indicadores
    Se incorpora la biblioteca 'react-countup' para animar los contadores en el componente Indicadores, mejorando la interactividad y la presentación visual. Además, se actualizan los archivos package.json y package-lock.json para incluir la nueva dependencia.

--- src/componentes/Indicadores.tsx
+++ src/componentes/Indicadores.tsx
@@ -1,10 +1,13 @@
+import CountUp from 'react-countup';
 
 const Indicadores = () => {
   return (
     <div className="w-11/12 max-w-4xl mx-auto flex flex-col gap-12 items-center justify-center md:flex-row md:gap-8 md:justify-between">
 
       <div className="flex items-center gap-2">
-        <p className="text-7xl font-bold text-white lg:text-8xl">94</p>
+        <p className="text-7xl font-bold text-white lg:text-8xl">
+          <CountUp end={94} duration={2.5} enableScrollSpy />
+        </p>
         <div className="pt-4">
           <p className="text-white">Commits</p>
           <p className="text-white">Realizados</p>
@@ -12,7 +15,9 @@ const Indicadores = () => {
       </div>
 
       <div className="flex items-center gap-2">
-        <p className="text-7xl font-bold text-white lg:text-8xl">32+</p>
+        <p className="text-7xl font-bold text-white lg:text-8xl">
+          <CountUp end={32} duration={2.5} enableScrollSpy suffix="+" />
+        </p>
         <div className="pt-4">
           <p className="text-white">Proyectos</p>
           <p className="text-white">Realizados </p>
@@ -20,7 +25,9 @@ const Indicadores = () => {
       </div>
 
       <div className="flex items-center gap-2">
-        <p className="text-7xl font-bold text-white lg:text-8xl">8</p>
+        <p className="text-7xl font-bold text-white lg:text-8xl">
+          <CountUp end={8} duration={2.5} enableScrollSpy />
+        </p>
         <div className="pt-4">
           <p className="text-white">Recomendaciones</p>
           <p className="text-white">Asegurados</p>
